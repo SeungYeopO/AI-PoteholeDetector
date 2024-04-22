5d4201f9acd7b93ddb2aa162e33565649c02a02f
yugyeong <starinsky284@gmail.com>
2024-04-22 17:32:56 +0900

    feat:환경설정,로그인페이지 라우터설정

---
+++ frontend/poppy/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from './pages/LoginPage';
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+  </BrowserRouter>
+
+  );
+};
+
+export default App;
---
+++ frontend/poppy/src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+
+
+const LoginPage = () => {
+  return (
+    <div>
+      안녕
+      
+    </div>
+  );
+};
+
+export default LoginPage;
