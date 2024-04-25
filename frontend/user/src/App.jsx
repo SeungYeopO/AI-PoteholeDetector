14e290ffd0ef2dfe53a01487992cc9ab1ed2f0dd
유명렬 <dlek567@gmail.com>
2024-04-25 12:09:00 +0900

    feat: 경로안내사항 출력

--- frontend/user/src/App.jsx
+++ frontend/user/src/App.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import Map from "./component/Map";
+import Report from "./component/Report";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Map />
+    <Router>
+      <div className="App">
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Map key={new Date()} />} />
+            <Route path="/report" exact element={<Report />} />
+          </Routes>
+        </div>
+        <Navbar />
+      </div>
+    </Router>
+  );
+}
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <Link to="/">홈</Link>
+      <Link to="/report">신고하기</Link>
+      <Link to="/reports">신고목록</Link>
+      <Link to="/blackbox">블랙박스</Link>
     </div>
   );
 }
