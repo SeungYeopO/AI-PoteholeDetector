21e409d32aa780a609efe63ad4e4d7079ff8e058
양성호 <tipan33@naver.com>
2024-04-30 10:10:11 +0900

    🐛fix: dummy data fix

---
+++ backend/poppy/src/main/java/com/h2o/poppy/DataLoader.java
@@ -0,0 +1,76 @@
+package com.h2o.poppy;
+
+import com.h2o.poppy.entity.*;
+import com.h2o.poppy.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ManagerRepository managerRepository;
+    @Autowired
+    private SerialListRepository serialListRepository;
+    @Autowired
+    private PotholeRepository potholeRepository;
+    @Autowired
+    private BlackboxVideoMetadataRepository blackboxVideoMetadataRepository;
+    @Autowired
+    private AccidentReportRepository accidentReportRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadDummyData();
+    }
+
+    private void loadDummyData() {
+        userRepository.saveAll(List.of(
+                new User("ssafy", "1", "오승엽", "01012345678"),
+                new User("dlek567", "2", "유명렬", "01090123456")));
+
+        managerRepository.saveAll(List.of(
+                new Manager("samsung", "1", "황유경", "01012345678"),
+                new Manager("dlek567", "2", "유명렬", "01090123456")));
+
+        serialListRepository.saveAll(List.of(
+                new SerialList("123456789"),
+                new SerialList("987654321")));
+
+        Date date1 = new Date(2022 - 1900, 3, 25, 14, 30, 0); // 2022년 4월 25일 오후 2시 30분
+        Date date2 = new Date(2022 - 1900, 5, 46, 22, 30, 0); // 2022년 4월 25일 오후 2시 30분
+
+        potholeRepository.saveAll(List.of(
+                new Pothole(35.202370, 126.810139, true, true, "광주광역시", "광산구", "하남산단6번로", date1),
+                new Pothole(37.501475, 127.039515, true, false, "서울특별시", "강남수", "테헤란로", date2)));
+
+        SerialList serialList1 = serialListRepository.findById(1L).orElse(null);
+        SerialList serialList2 = serialListRepository.findById(2L).orElse(null);
+
+        // Create instances of BlackboxVideoMetadata and save them
+        if (serialList1 != null && serialList2 != null) {
+            blackboxVideoMetadataRepository.saveAll(List.of(
+                    new BlackboxVideoMetadata(serialList1, new Date(), 35.202370, 126.810139),
+                    new BlackboxVideoMetadata(serialList2, new Date(), 37.501475, 127.039515)));
+        }
+
+        User user1 = userRepository.findById(1L).orElse(null);
+        User user2 = userRepository.findById(2L).orElse(null);
+
+        Pothole pothole1 = potholeRepository.findById(1L).orElse(null);
+        Pothole pothole2 = potholeRepository.findById(2L).orElse(null);
+
+        BlackboxVideoMetadata BlackboxVideoMetadata1 = blackboxVideoMetadataRepository.findById(1L).orElse(null);
+        BlackboxVideoMetadata BlackboxVideoMetadata2 = blackboxVideoMetadataRepository.findById(2L).orElse(null);
+
+        accidentReportRepository.saveAll(List.of(
+                new AccidentReport(user1, pothole1, BlackboxVideoMetadata1, "광주신고함", true),
+                new AccidentReport(user2, pothole2, BlackboxVideoMetadata2, "서울신고함", false)));
+    }
+}
--- backend/poppy/src/main/java/com/h2o/poppy/controller/UserController.java
+++ backend/poppy/src/main/java/com/h2o/poppy/controller/UserController.java
@@ -1,87 +1,23 @@
 package com.h2o.poppy.controller;
 
-import com.h2o.poppy.entity.*;
+import com.h2o.poppy.entity.User;
 import com.h2o.poppy.model.user.UserDto;
-import com.h2o.poppy.repository.*;
-import com.h2o.poppy.service.*;
+import com.h2o.poppy.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import lombok.Getter;
 import java.util.List;
-import java.util.Date;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
-    private final ManagerRepository managerRepository;
-    private final SerialListService serialListService;
-    private final SerialListRepository serialListRepository;
-    private final AccidentReportRepository accidentReportRepository;
-    private final AccidentReportService accidentReportService;
-    private final UserRepository userRepository;
-    private final PotholeRepository potholeRepository;
-    private final BlackboxVideoMetadataRepository blackboxVideoMetadataRepository;
 
     @Autowired
-    public UserController(UserService userService, UserRepository userRepository,
-            AccidentReportService accidentReportService, AccidentReportRepository accidentReportRepository,
-            PotholeRepository potholeRepository, BlackboxVideoMetadataRepository blackboxVideoMetadataRepository,
-            SerialListService serialListService, SerialListRepository serialListRepository,
-            ManagerRepository managerRepository) {
+    public UserController(UserService userService) {
         this.userService = userService;
-        this.managerRepository = managerRepository;
-        this.serialListService = serialListService;
-        this.accidentReportService = accidentReportService;
-        this.accidentReportRepository = accidentReportRepository;
-        this.userRepository = userRepository;
-        this.potholeRepository = potholeRepository;
-        this.blackboxVideoMetadataRepository = blackboxVideoMetadataRepository;
-        this.serialListRepository = serialListRepository;
-
-        userRepository.saveAll(List.of(
-                new User("ssafy", "1", "오승엽", "01012345678"),
-                new User("dlek567", "2", "유명렬", "01090123456")));
-
-        managerRepository.saveAll(List.of(
-                new Manager("samsung", "1", "황유경", "01012345678"),
-                new Manager("dlek567", "2", "유명렬", "01090123456")));
-
-        serialListRepository.saveAll(List.of(
-                new SerialList("123456789"),
-                new SerialList("987654321")));
-
-        Date date1 = new Date(2022 - 1900, 3, 25, 14, 30, 0); // 2022년 4월 25일 오후 2시 30분
-        Date date2 = new Date(2022 - 1900, 5, 46, 22, 30, 0); // 2022년 4월 25일 오후 2시 30분
-
-        potholeRepository.saveAll(List.of(
-                new Pothole(35.202370, 126.810139, true, true, "광주광역시", "광산구", "하남산단6번로", date1),
-                new Pothole(37.501475, 127.039515, true, false, "서울특별시", "강남수", "테헤란로", date2)));
-
-        SerialList serialList1 = serialListRepository.findById(1L).orElse(null);
-        SerialList serialList2 = serialListRepository.findById(2L).orElse(null);
-
-        // Create instances of BlackboxVideoMetadata and save them
-        if (serialList1 != null && serialList2 != null) {
-            blackboxVideoMetadataRepository.saveAll(List.of(
-                    new BlackboxVideoMetadata(serialList1, new Date(), 35.202370, 126.810139),
-                    new BlackboxVideoMetadata(serialList2, new Date(), 37.501475, 127.039515)));
-        }
-
-        User user1 = userRepository.findById(1L).orElse(null);
-        User user2 = userRepository.findById(2L).orElse(null);
-
-        Pothole pothole1 = potholeRepository.findById(1L).orElse(null);
-        Pothole pothole2 = potholeRepository.findById(2L).orElse(null);
-
-        BlackboxVideoMetadata BlackboxVideoMetadata1 = blackboxVideoMetadataRepository.findById(1L).orElse(null);
-        BlackboxVideoMetadata BlackboxVideoMetadata2 = blackboxVideoMetadataRepository.findById(2L).orElse(null);
-
-        accidentReportRepository.saveAll(List.of(
-                new AccidentReport(user1, pothole1, BlackboxVideoMetadata1, "광주신고함", true),
-                new AccidentReport(user2, pothole2, BlackboxVideoMetadata2, "서울신고함", false)));
     }
 
     // 전체 유저 읽기
@@ -98,7 +34,7 @@ public class UserController {
 
     // 아이디 중복 검사
     @PostMapping("/duplicate-id")
-    public Object DuplicateId(@RequestBody UserDto data) {
+    public Object duplicateId(@RequestBody UserDto data) {
         boolean result = userService.duplicateId(data.getLoginId());
         @Getter
         class duplicateIdResponse {
@@ -117,7 +53,6 @@ public class UserController {
         long userPk = userService.saveData(data);
         boolean success = userPk > 0; // PK가 0보다 크다면 성공으로 간주
 
-        // 메서드 내 로컬 클래스 정의
         @Getter
         class SaveResponse {
             private final boolean success;
@@ -128,15 +63,13 @@ public class UserController {
                 this.userPk = userPk;
             }
         }
-        // 로컬 클래스 인스턴스 생성 및 반환
         return new SaveResponse(success, userPk);
     }
 
     // 수정
     @PutMapping
     public Object updateData(@RequestBody UserDto data) {
-        int userPk = userService.updateData(data);
-        // 1 이면 2개다 수정 2 이면 비번만 3이면 전번만 0이면 아무것도 안바꿈
+        long userPk = userService.updateData(data);
         @Getter
         class UpdateDataResponse {
             private final boolean result;
@@ -154,19 +87,20 @@ public class UserController {
         return userService.deleteData(userPk);
     }
 
+    // 로그인
     @PostMapping("/login")
     public Object login(@RequestBody UserDto data) {
         long userPk = userService.login(data);
         @Getter
-        class loginResponse {
+        class LoginResponse {
             private final boolean result;
             private final long userPk;
 
-            loginResponse(long userPk) {
+            LoginResponse(long userPk) {
                 this.userPk = userPk;
                 this.result = userPk != 0;
             }
         }
-        return new loginResponse(userPk);
+        return new LoginResponse(userPk);
     }
 }
