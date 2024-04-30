ca131a24c4e20117ec58e7f7d8c23438b8d9986a
선상범 <gksgusrhks12@naver.com>
2024-04-30 10:39:57 +0900

    add:사고등록조회

--- backend/poppy/src/main/java/com/h2o/poppy/repository/AccidentReportRepository.java
+++ backend/poppy/src/main/java/com/h2o/poppy/repository/AccidentReportRepository.java
@@ -1,11 +1,19 @@
 package com.h2o.poppy.repository;
 
 import com.h2o.poppy.entity.AccidentReport;
+import com.h2o.poppy.entity.Pothole;
+import com.h2o.poppy.entity.User;
+import com.h2o.poppy.model.accidentreport.AccidentReportDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface AccidentReportRepository extends JpaRepository<AccidentReport, Long> {
-    //List<AccidentReport> findByUserPk(User userPk);
+
+    @Query("SELECT new com.h2o.poppy.model.accidentreport.AccidentReportDto(ar.reportPk, ar.userPk.userPk, ar.potholePk.potholePk, ar.videoPk.videoPk, ar.reportContent, ar.isProcess) FROM AccidentReport ar WHERE ar.userPk.userPk = :userId")
+    List<AccidentReportDto> getAccidentReportInfoByUserId(@Param("userId") Long userId);
 
 }
