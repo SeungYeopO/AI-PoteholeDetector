15a0a901eeb4abf1fc5bf42655b20c22b2479325
yugyeong <starinsky284@gmail.com>
2024-05-08 16:43:01 +0900

    feat: 페이지 화면 구성 1차 완료

--- frontend/user/dev-dist/sw.js
+++ frontend/user/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2ejbnrtg668"
+    "revision": "0.fn99vsj5oh"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
