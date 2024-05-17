463e78adad872d2b42c4bb954b095dc02beeca37
선상범 <gksgusrhks12@naver.com>
2024-05-17 16:17:43 +0900

    Update DataLoader.java

--- backend/poppy/src/main/java/com/h2o/poppy/DataLoader.java
+++ backend/poppy/src/main/java/com/h2o/poppy/DataLoader.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
 
@@ -36,80 +38,64 @@ public class DataLoader implements CommandLineRunner {
 
     private void loadDummyData() {
         userRepository.saveAll(List.of(
-                new User("ssafy", "1", "오승엽", "01012345678"),
-                new User("dlek567", "2", "유명렬", "01090123456"),
-                new User("gggg", "2", "김정은", "01095253456")));
+                new User("ssafy", "1", "싸피", "01095253456")));
 
         managerRepository.saveAll(List.of(
-                new Manager("samsung", "1", "황유경", "01012345678"),
-                new Manager("dlek567", "2", "유명렬", "01090123456")));
+                new Manager("ssafy", "1", "싸피", "01095253456")));
 
         serialListRepository.saveAll(List.of(
-                new SerialList("BB12345678"),
-                new SerialList("BB47125156"),
-                new SerialList("BB98765432")));
+                new SerialList("BB47125156")));
 
         User user1 = userRepository.findById(1L).orElse(null);
-        User user2 = userRepository.findById(2L).orElse(null);
-        User user3 = userRepository.findById(3L).orElse(null);
         SerialList serial1 = serialListRepository.findById(1L).orElse(null);
-        SerialList serial2 = serialListRepository.findById(2L).orElse(null);
-        SerialList serial3 = serialListRepository.findById(3L).orElse(null);
 
         usersSerialsRepository.saveAll(List.of(
-                new UsersSerials(user1, serial1),
-                new UsersSerials(user3, serial3),
-                new UsersSerials(user2, serial2)));
+                new UsersSerials(user1, serial1)));
 
-        Date now = new Date();
-        Date date1 = new Date(2022 - 1900, 3, 25, 14, 30, 0);
-        Date date2 = new Date(2022 - 1900, 5, 46, 22, 30, 0); 
-        Date date3 = new Date(2024 - 1900, 3, 10, 22, 30, 0); 
-        Date date4 = new Date(2024 - 1900, 4, 16, 22, 30, 0); 
+
+        LocalDateTime now = LocalDateTime.now();
+
+        LocalDateTime detectedDateTime = now.plusDays(-3);
+        LocalDateTime expectDateTime = now.plusDays(4);
+        LocalDateTime completeStartDateTime = now.plusDays(-7);
+        LocalDateTime completeDateTime = now.plusDays(-10);
+
+        Date currentDate = Date.from(now.atZone(ZoneId.systemDefault()).toInstant());
+        Date detectedDate = Date.from(detectedDateTime.atZone(ZoneId.systemDefault()).toInstant());
+        Date expectDate = Date.from(expectDateTime.atZone(ZoneId.systemDefault()).toInstant());
+        Date completeStartDate = Date.from(completeStartDateTime.atZone(ZoneId.systemDefault()).toInstant());
+        Date completeDate = Date.from(completeDateTime.atZone(ZoneId.systemDefault()).toInstant());
+
 
         potholeRepository.saveAll(List.of(
-                new Pothole(35.202370, 126.810139, true, "광주", "광산구", "하남산단6번로", date1, "미확인",null,null,null,null),
-                new Pothole(37.501475, 127.039515, true, "서울", "강남구", "테헤란로", date2, "미확인",null,null,null,null),
-                new Pothole(33.555664, 126.796320, true, "제주", "제주시", "해맞이해안로", date2, "미확인",null,null,null,null),
-                new Pothole(38.181005, 128.549175, true, "강원도", "속초시", "동해 고속도로", date2, "미확인",null,null,null,null),
-                new Pothole(35.1990838,  129.2066338, true, "부산", "기장군", "읍내로", date2, "공사완료",date3,null,now,null),
-                new Pothole(35.897045,  128.851677, true, "경상북도", "경산시", "대구대로", date2, "공사완료",date3,null,now,null),
-                new Pothole(37.467655,  126.946325, true, "서울", "관악구", "관악로", date2, "공사완료",date3,null,now,null),
-                new Pothole(36.334859, 126.620023 , true, "충청남도", "보령시", "옥마로", date2, "공사완료",date3,null,now,null),
-                new Pothole(36.102575,  127.499691, true, "충청남도", "금산군", "인삼광장로", date2, "공사완료",date3,null,now,null),
-                new Pothole(37.880579, 127.729987, true, "강원도", "춘천시", "금강로", date2, "미확인",null,null,null,null),
-                new Pothole(37.456733, 126.705120, false, "인천", "남동구", "정각로", date2, "확인전",now,date4,null,null),
-                new Pothole(36.327580, 126.427247, false, "대전", "중구", "대종로", date2, "확인전",now,date4,null,null),
-                new Pothole(35.216768, 128.142013, true, "경상남도", "진주시", "금산로", date2, "공사중",now,date4,null,null),
-                new Pothole(37.130705,128.537282 , true, "강원도", "영월군", "영월동로", date2, "공사중",now,date4,null,null),
-                new Pothole(36.098999 ,127.490736 , true, "충청남도", "금산군", "비단로", date2, "공사중",now,date4,null,null),
-                new Pothole(36.099268 ,127.483175 , false, "충청남도", "금산군", "비단로", date2, "확인전",now,date4,null,"얀녕하세요얀녕하세요얀녕하세요얀녕하세요얀녕하세요얀녕하세요"),
-                new Pothole(36.642488, 127.489675, false, "충청북도", "청주시", "상당로", date2, "확인전",now,date4,null,"ssfaegsserhaseaeuhgasehgoerhgorhgerhthjrshdthtjtjtjr")));
+                new Pothole(35.202370, 126.810139, true, "광주", "광산구", "하남산단6번로", detectedDate, "미확인",null,null,null,null),
+                new Pothole(37.501475, 127.039515, true, "서울", "강남구", "테헤란로", currentDate, "미확인",null,null,null,null),
+                new Pothole(33.555664, 126.796320, true, "제주", "제주시", "해맞이해안로", currentDate, "미확인",null,null,null,null),
+                new Pothole(35.199083,  129.206633, true, "부산", "기장군", "기장대로", completeDate, "공사완료",completeStartDate,detectedDate,currentDate,null),
+                new Pothole(35.897045,  128.851677, true, "경북", "경산시", "대구대로", completeDate, "공사완료",completeStartDate,detectedDate,currentDate,null),
+                new Pothole(37.467655,  126.946325, true, "서울", "관악구", "강남순환도시", completeDate, "공사완료",completeStartDate,detectedDate,currentDate,null),
+                new Pothole(36.102575,  127.499691, true, "충남", "금산군", "인삼로", completeDate, "공사완료",completeStartDate,detectedDate,currentDate,null),
+                new Pothole(37.880579, 127.729987, true, "강원", "춘천시", "시청길", currentDate, "미확인",null,null,null,null),
+                new Pothole(37.456733, 126.705120, false, "인천", "남동구", "구월로", currentDate, "사용자등록",null,null,null,null),
+                new Pothole(35.216768, 128.142013, true, "경남", "진주시", "중천로", detectedDate, "공사중",currentDate,expectDate,null,null),
+                new Pothole(36.098999 ,127.490736 , true, "충남", "금산군", "비단로", detectedDate, "공사중",currentDate,expectDate,null,null),
+                new Pothole(36.099268 ,127.483175 , false, "충남", "금산군", "비단로", detectedDate, "사용자등록",null,null,null,null),
+                new Pothole(36.642488, 127.489675, false, "충북", "청주시", "상당구", currentDate, "사용자등록",null,null,null,null)));
 
         SerialList serialList1 = serialListRepository.findById(1L).orElse(null);
-        SerialList serialList2 = serialListRepository.findById(2L).orElse(null);
-        SerialList serialList3 = serialListRepository.findById(3L).orElse(null);
 
-        if (serialList1 != null && serialList2 != null && serialList3 != null) {
+
+        if (serialList1 != null ) {
             blackboxVideoMetadataRepository.saveAll(List.of(
-                    new BlackboxVideoMetadata(serialList1, now, 35.202370, 126.810139, null),
-                    new BlackboxVideoMetadata(serialList1, now, 33.555664, 126.796320, null),
-                    new BlackboxVideoMetadata(serialList1, now, 38.181005, 128.549175, null),
-                    new BlackboxVideoMetadata(serialList1, now, 35.1990838,  129.2066338, null),
-                    new BlackboxVideoMetadata(serialList1, now, 35.897045,  128.851677, null),
-                    new BlackboxVideoMetadata(serialList1, now, 37.467655,  126.946325, null),
-                    new BlackboxVideoMetadata(serialList1, now, 36.334859, 126.620023, null),
-                    new BlackboxVideoMetadata(serialList2, now, 36.102575,  127.499691, null),
-                    new BlackboxVideoMetadata(serialList2, now, 37.456733, 126.705120, null),
-                    new BlackboxVideoMetadata(serialList2, now, 36.327580, 126.427247, null),
-                    new BlackboxVideoMetadata(serialList2, now, 35.216768, 128.142013, null),
-                    new BlackboxVideoMetadata(serialList3, now, 37.130705,128.537282, null),
-                    new BlackboxVideoMetadata(serialList3, now, 36.642488, 127.489675, null),
-                    new BlackboxVideoMetadata(serialList2, now, 37.880579, 127.729987, null),
-                    new BlackboxVideoMetadata(serialList1, now, 36.098999, 127.490736, null),
-                    new BlackboxVideoMetadata(serialList2, now, 36.099268, 127.483175, null),
-                    new BlackboxVideoMetadata(serialList2, now, 36.092268, 127.483175, null),
-                    new BlackboxVideoMetadata(serialList2, now, 37.501475, 127.039515, null)));
+                    new BlackboxVideoMetadata(serialList1, currentDate, 35.202370, 126.810139, null),
+                    new BlackboxVideoMetadata(serialList1, currentDate, 37.501475, 127.039515, null),
+                    new BlackboxVideoMetadata(serialList1, currentDate, 33.555664, 126.796320, null),
+                    new BlackboxVideoMetadata(serialList1, currentDate, 35.199083,  129.206633, null),
+                    new BlackboxVideoMetadata(serialList1, detectedDate, 35.897045,  128.851677, null),
+                    new BlackboxVideoMetadata(serialList1, detectedDate, 37.467655,  126.946325, null),
+                    new BlackboxVideoMetadata(serialList1, detectedDate, 36.102575,  127.499691, null),
+                    new BlackboxVideoMetadata(serialList1, detectedDate, 37.880579,  127.729987, null),
+                    new BlackboxVideoMetadata(serialList1, detectedDate, 37.456733, 126.705120, null)));
         }
 
         Pothole pothole1 = potholeRepository.findById(1L).orElse(null);
@@ -121,12 +107,6 @@ public class DataLoader implements CommandLineRunner {
         Pothole pothole7 = potholeRepository.findById(7L).orElse(null);
         Pothole pothole8 = potholeRepository.findById(8L).orElse(null);
         Pothole pothole9 = potholeRepository.findById(9L).orElse(null);
-        Pothole pothole10 = potholeRepository.findById(10L).orElse(null);
-        Pothole pothole11 = potholeRepository.findById(11L).orElse(null);
-        Pothole pothole12 = potholeRepository.findById(12L).orElse(null);
-        Pothole pothole13 = potholeRepository.findById(13L).orElse(null);
-        Pothole pothole14 = potholeRepository.findById(14L).orElse(null);
-        Pothole pothole15 = potholeRepository.findById(15L).orElse(null);
 
         BlackboxVideoMetadata BlackboxVideoMetadata1 = blackboxVideoMetadataRepository.findById(1L).orElse(null);
         BlackboxVideoMetadata BlackboxVideoMetadata2 = blackboxVideoMetadataRepository.findById(2L).orElse(null);
@@ -137,32 +117,16 @@ public class DataLoader implements CommandLineRunner {
         BlackboxVideoMetadata BlackboxVideoMetadata7 = blackboxVideoMetadataRepository.findById(7L).orElse(null);
         BlackboxVideoMetadata BlackboxVideoMetadata8 = blackboxVideoMetadataRepository.findById(8L).orElse(null);
         BlackboxVideoMetadata BlackboxVideoMetadata9 = blackboxVideoMetadataRepository.findById(9L).orElse(null);
-        BlackboxVideoMetadata BlackboxVideoMetadata10 = blackboxVideoMetadataRepository.findById(10L).orElse(null);
-        BlackboxVideoMetadata BlackboxVideoMetadata11 = blackboxVideoMetadataRepository.findById(11L).orElse(null);
-        BlackboxVideoMetadata BlackboxVideoMetadata12 = blackboxVideoMetadataRepository.findById(12L).orElse(null);
-        BlackboxVideoMetadata BlackboxVideoMetadata13 = blackboxVideoMetadataRepository.findById(13L).orElse(null);
-        BlackboxVideoMetadata BlackboxVideoMetadata14 = blackboxVideoMetadataRepository.findById(14L).orElse(null);
-        BlackboxVideoMetadata BlackboxVideoMetadata15 = blackboxVideoMetadataRepository.findById(15L).orElse(null);
-        BlackboxVideoMetadata BlackboxVideoMetadata16 = blackboxVideoMetadataRepository.findById(16L).orElse(null);
-        BlackboxVideoMetadata BlackboxVideoMetadata17 = blackboxVideoMetadataRepository.findById(17L).orElse(null);
 
         accidentReportRepository.saveAll(List.of(
-                new AccidentReport(user1, pothole1, BlackboxVideoMetadata1, "1번제목","1번내용", date1, "미확인",null),
-                new AccidentReport(user1, pothole2, BlackboxVideoMetadata2, "2번제목","2번내용", date1, "미확인",null),
-                new AccidentReport(user1, pothole3, BlackboxVideoMetadata3, "3번제목","3번내용", date1, "미확인",null),
-                new AccidentReport(user1, pothole4, BlackboxVideoMetadata4, "4번제목","4번내용", date1, "미확인",null),
-                new AccidentReport(user1, pothole5, BlackboxVideoMetadata5, "5번제목","5번내용", date1, "미확인",null),
-                new AccidentReport(user1, pothole6, BlackboxVideoMetadata6, "6번제목","6번내용", date1, "미확인",null),
-                new AccidentReport(user1, pothole7, BlackboxVideoMetadata7, "7번제목","7번내용", date2, "반려","그냥"),
-                new AccidentReport(user2, pothole8, BlackboxVideoMetadata8, "8번제목","8번내용", date2, "반려","그냥"),
-                new AccidentReport(user2, pothole9, BlackboxVideoMetadata9, "9번제목","9번내용", date2, "반려","그냥"),
-                new AccidentReport(user2, pothole10, BlackboxVideoMetadata10, "10번제목","10번내용", date2, "반려","그냥"),
-                new AccidentReport(user2, pothole11, BlackboxVideoMetadata11, "11번제목","11번내용", date2, "미확인",null),
-                new AccidentReport(user2, pothole12, BlackboxVideoMetadata12, "12번제목","12번내용", date2, "보상완료",null),
-                new AccidentReport(user2, pothole13, BlackboxVideoMetadata13, "13번제목","13번내용", date2, "보상완료",null),
-                new AccidentReport(user2, pothole14, BlackboxVideoMetadata14, "14번제목","14번내용", date2, "보상완료",null),
-                new AccidentReport(user2, pothole14, BlackboxVideoMetadata15, "15번제목","15번내용", date2, "보상완료",null),
-                new AccidentReport(user2, pothole14, BlackboxVideoMetadata16, "16번제목","16번내용", date2, "미확인",null),
-                new AccidentReport(user2, pothole15, BlackboxVideoMetadata17, "17번제목","17번내용",date2,"반려","그냥")));
+                new AccidentReport(user1, pothole1, BlackboxVideoMetadata1, "포트홀사고","포트홀 때문에 타이어 터졌어요", currentDate, "미확인",null),
+                new AccidentReport(user1, pothole2, BlackboxVideoMetadata2, "사고신고","포트홀 피하려다가 다른차랑 충돌했어요, 보상해주세요.", currentDate, "미확인",null),
+                new AccidentReport(user1, pothole3, BlackboxVideoMetadata3, "사고","이런것도 보상받을수 있나요?", currentDate, "미확인",null),
+                new AccidentReport(user1, pothole4, BlackboxVideoMetadata4, "신고","안녕하세요. 포트홀에 의한 사고보상 요청드립니다.", currentDate, "미확인",null),
+                new AccidentReport(user1, pothole5, BlackboxVideoMetadata5, "포트홀","포트홀좀 공사해라", detectedDate, "보상완료",null),
+                new AccidentReport(user1, pothole6, BlackboxVideoMetadata6, "충돌사고","포트홀 때문에 타이어 펑크났어요", detectedDate, "보상완료",null),
+                new AccidentReport(user1, pothole7, BlackboxVideoMetadata7, "포트홀","포트홀에 지나간 이후부터 차에 에어컨이 안나와요.", detectedDate, "반려","포트홀이 없습니다."),
+                new AccidentReport(user1, pothole8, BlackboxVideoMetadata8, "사고남","앞차가 갑자기 뒤로옴", detectedDate, "반려","포트홀이 없습니다."),
+                new AccidentReport(user1, pothole9, BlackboxVideoMetadata9, "보상해줘","보상해줘", detectedDate, "반려","영상이 인정되지 않았습니다.")));
     }
 }
